9b6a737e9317f59006514b12061a390a8917a86d
Stela Palii <stelasimonapalii@gmail.com>
2022-12-10 11:21:21 -0800

    Implement die roll from user input as sides

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,64 @@
 import * as ReactDOM from 'react-dom/client';
 import React, { ChangeEvent, useState } from 'react';
 
+import { validatePosInt } from '../server/utils';
+
+const PATH = '/api/v1/rolldie/';
+
 function AppRoot() {
-  //   const [];
-  // add state here
-  return <>Hello World</>;
+  const [dieValue, setDieValue] = useState(0);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [sidesAsString, setSidesAsString] = useState('');
+
+  const sidesAsInt = validatePosInt(sidesAsString);
+  const canSubmit = Number.isNaN(sidesAsInt) || sidesAsInt == null;
+
+  const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setSidesAsString(e.target.value);
+  };
+
+  const onSubmit = canSubmit
+    ? undefined
+    : async () => {
+        setErrorMessage(null);
+        setIsSubmitting(true);
+
+        try {
+          const response = await fetch(PATH, {
+            method: 'POST',
+            body: JSON.stringify({ dieSides: sidesAsInt }),
+          });
+          const data = await response.json();
+          setDieValue(data.roll);
+        } catch (err) {
+          setErrorMessage((err as Error).message);
+          console.error('Error fetching the die sides', err);
+        }
+        setIsSubmitting(false);
+      };
+
+  const isDisabled = onSubmit == null || isSubmitting;
+
+  return (
+    <>
+      Die roll
+      <input
+        type="text"
+        placeholder="type die sides"
+        value={sidesAsString}
+        onChange={onChangeValue}
+        disabled={isSubmitting}
+      />
+      <button onClick={onSubmit} disabled={isDisabled}>
+        Submit
+      </button>
+      {dieValue !== 0 && <div>{`Die roll value: ${dieValue}`} </div>}
+      {errorMessage != null && (
+        <div>{`An error has occured: ${errorMessage}`}</div>
+      )}
+    </>
+  );
 }
 
 function main() {
--- src/server/main.tsx
+++ src/server/main.tsx
@@ -2,9 +2,36 @@ import express from 'express';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import fs from 'fs';
+import { dieRoll } from './utils';
 
 const app = express();
 const PORT = 5000;
+
+app.use((req, res, next) => {
+  console.log('Logging path:', req.path);
+  next();
+});
+
+app.use((req, res, next) => {
+  if (req.method !== 'POST') {
+    next();
+    return;
+  }
+  console.log('Parsing body');
+  const data: string[] = [];
+  req.on('data', (d: Buffer) => {
+    data.push(d.toString());
+  });
+  req.on('end', () => {
+    try {
+      req.body = JSON.parse(data.join(''));
+    } catch (err) {
+      console.log('Logging error for body parsing:', err);
+      req.body = {};
+    }
+    next();
+  });
+});
 
 app.get('/', (req, res) => {
   res.send(
@@ -28,6 +55,14 @@ app.get('/index.js', (req, res) => {
   const data = fs.readFileSync('build/main.js', 'utf8');
 
   res.send(data);
+});
+
+const ROLL_DIE_PATH = '/api/v1/rolldie/';
+app.post(ROLL_DIE_PATH, (req, res, next) => {
+  const { dieSides } = req.body;
+  res.type('application/json');
+  const roll = dieRoll(dieSides ?? 6);
+  res.send(JSON.stringify({ roll }));
 });
 
 app.listen(PORT, () => {
